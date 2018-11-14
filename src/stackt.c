7b458c05bad0f0130106e19c9469400279acd93d
ikraduya <ikraduya@gmail.com>
2018-11-14 10:34:09 +0700

    add validator

---
+++ src/stackt.c
@@ -0,0 +1,24 @@
+#include "../include/stackt.h"
+
+void CreateEmpty (Stack *S){
+    Top(*S) = Nil;
+    InfoTop(*S) = Nil;
+}
+
+boolean IsEmpty (Stack S){
+    return (Top(S) == Nil);
+}
+
+boolean IsFull (Stack S){
+    return (Top(S) == MaxEl);
+}
+
+void Push (Stack * S, infotype X){
+    Top(*S)++;
+    InfoTop(*S) = X;
+}
+
+void Pop (Stack * S, infotype* X){
+    *X = InfoTop(*S);
+    Top(*S)--;
+}
